587f0b3689efabdc0eb66df8568b7498103ee320
amos-kibet <kibetamos97@gmail.com>
2022-12-12 20:48:40 +0300

    fix: initialize variables

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-userSchema = new mongoose.Schema({
-	username: {type: String, required: true},
-	password: {type: String},
-	courses:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'course' }],
-  });
+var userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  // name: { type: String, required: false },
+  // email: { type: String, required: false },
+  password: { type: String, required: true },
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "course" }],
+});
 
-var passportLocalMongoose = require('passport-local-mongoose');
+var passportLocalMongoose = require("passport-local-mongoose");
 userSchema.plugin(passportLocalMongoose);
-user = mongoose.model('user', userSchema);
+var user = mongoose.model("user", userSchema);
 module.exports = user;
-
-
